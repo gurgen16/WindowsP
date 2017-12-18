e4d7c74b288a493f435866349181a2dbc660b66e
gurgen16 <rommesbob@hotmail.com>
2017-12-18 16:03:20 +0100

    Added pyhk and all the code to this point

---
+++ WinP.py
@@ -0,0 +1,21 @@
+import pyhk
+import webbrowser
+import pyautogui
+import win32gui
+
+iPath=r'C:\Users\Bob-Lap\Pictures\2300857-star-wars-kit-fisto-coloring-pages.png'
+hot = pyhk.pyhk()
+pyautogui.
+def FloatyP():
+    print('Jop')
+    with webbrowser.open(iPath) as P:
+
+
+
+def Stopo():
+    hot.end()
+
+
+winp=hot.addHotkey(['Win','P'],FloatyP)
+stop=hot.addHotkey(['P'],Stopo)
+hot.start()
