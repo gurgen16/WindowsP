398bed49451a8622bb2bdd282765c1347dd7b157
Bob <rommesbob@hotmail.com>
2018-01-05 00:42:15 +0100

    added random speed

--- WinP.py
+++ WinP.py
@@ -5,7 +5,9 @@ import win32gui
 import win32con
 import os.path
 import time
+import random
 
+random.seed()
 # iPath=r'C:\Users\Bob-Lap\Pictures\2300857-star-wars-kit-fisto-coloring-pages.png'
 pPath=os.path.abspath("Penis.jpg")
 
@@ -26,12 +28,21 @@ def FloatyP():
     h=700
     x = 20
     y = 20
-    x_d = 2
-    y_d = 2
+    x_d=2
+    y_d=2
     x_r=1
     y_r=1
     yo=True
+    count=1
+    change=0
     while yo==True:
+
+        if count>change:
+            speed=1/random.randint(100,5000)
+            change=random.randint(25,1000)
+            print(speed,change)
+
+            count=0
         if (x+x_d+b>=2560)or(x+x_d<=0):
             x_d = x_d*(-1)
         if (y+y_d+h>=1440)or(y+y_d<=0):
@@ -39,12 +50,14 @@ def FloatyP():
         x=x+x_d
         y=y+y_d
         win32gui.MoveWindow(foto, x, y, b, h,True)
-        time.sleep(0.01)
 
-def Stopo():
-    hot.end()
+        time.sleep(speed)
+        count += 1
+
+# def Stopo():
+#     hot.end()
 
 
 winp=hot.addHotkey(['Win','P'],FloatyP)
-stop=hot.addHotkey(['P'],Stopo)
+# stop=hot.addHotkey(['P'],Stopo)
 hot.start()
